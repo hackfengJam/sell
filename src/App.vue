41b557cf634ebe37d5408a52e69c6f8a22d148e5
HackFun <hackfeng624@gmail.com>
2021-04-06 01:00:41 +0800

    feat: test

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,18 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view/>
+    <test></test>
   </div>
 </template>
 
 <script>
+import test from './components/test'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    test: test
+  }
 }
 </script>
 
---
+++ src/components/test.vue
@@ -0,0 +1,25 @@
+<template>
+  <h2 class="red">test...{{ msg }}</h2>
+</template>
+
+<script>
+export default {
+  name: 'test',
+  props: {
+    msg: {
+      type: String,
+      default: 'test msg'
+    }
+  },
+  methods: {},
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+.red {
+  color: #FF0000
+}
+</style>
