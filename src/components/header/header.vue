ea7c856070c6ef879a7b7dfb5fa2de02273bc24b
HackFun <hackfeng624@gmail.com>
2021-07-06 23:32:07 +0800

    feat: 6-5 ~ 6-8

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -13,13 +13,23 @@
           {{ seller.description }}/{{ seller.deliveryTime }}分钟送达
         </div>
         <div v-if="seller.supports" class="support">
-          <span class="icon"></span>
-          <span class="text">{{ seller.supports[0].description }}</span>
+          <span class="icon" :class="this.classMap[seller.supports[0].type]"></span><span
+          class="text">{{ seller.supports[0].description }}</span>
         </div>
       </div>
-
+      <div v-if="seller.supports" class="support-count">
+        <span class="count">{{ seller.supports.length }}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
     </div>
-    <div class="bulletin-wrapper"></div>
+    <div class="bulletin-wrapper">
+      <span class="bulletin-title"></span>
+      <span class="bulletin-text">{{ seller.bulletin }}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
   </div>
 </template>
 
@@ -29,6 +39,15 @@ export default {
     seller: {
       type: Object
     }
+  },
+  data () {
+    return {
+      imgSrc: ''
+    }
+  },
+  created () {
+    this.classMap = ['decrease', 'discount', 'special', 'guarantee', 'invoice']
+    this.imgSrc = this.seller.avatar
   }
 }
 </script>
@@ -37,21 +56,27 @@ export default {
 @import "../../common/stylus/mixin.styl"
 
 .header
+  position: relative
   color: #fff
-  background: #000
+  background: rgba(7, 17, 27, 0.5)
+  overflow: hidden
 
   .content-wrapper
+    position: relative
     // 上 右 下 左
     padding: 24px 12px 18px 24px
     font-size: 0
 
     .avatar
       display: inline-block
+      vertical-align: top
+
+      img
+        border-radius: 2px
 
     .content
       display: inline-block
       margin-left: 16px
-      font-size: 14px
 
       .title
         margin: 2px 0 8px 0
@@ -71,4 +96,100 @@ export default {
           line-height: 18px
           font-weight: bold
 
+      .description
+        margin-bottom: 10px
+        line-height: 12px
+        font-size: 12px
+
+      .support
+        .icon
+          display: inline-block
+          vertical-align: top
+          width: 12px
+          height: 12px
+          margin-right: 4px
+          background-size: 12px
+          background-repeat: no-repeat
+
+          &.decrease
+            bg-image('decrease_1')
+
+          &.discount
+            bg-image('discount_1')
+
+          &.guarantee
+            bg-image('guarantee_1')
+
+          &.invoice
+            bg-image('invoice_1')
+
+          &.special
+            bg-image('special_1')
+
+        .text
+          line-height: 12px
+          font-size: 10px
+
+    .support-count
+      position: absolute
+      right: 12px
+      bottom: 14px
+      padding: 0 8px
+      height: 24px
+      line-height: 24px
+      border-radius: 14px
+      background: rgba(0, 0, 0, 0.2)
+      text-align: center
+
+      .count
+        // line-height 必须等于 height
+        vertical-align: top
+        font-size: 10px
+
+      .icon-keyboard_arrow_right
+        margin-left: 2px
+        // line-height 必须等于 height
+        line-height: 24px
+        font-size: 10px
+
+  .bulletin-wrapper
+    position: relative
+    height: 28px
+    line-height: 28px
+    padding: 0 22px 0 12px
+    white-space: nowrap
+    overflow: hidden
+    text-overflow: ellipsis
+    background: rgba(7, 17, 27, 0.2)
+
+    .bulletin-title
+      display: inline-block
+      vertical-align: top
+      margin-top: 8px
+      width: 22px
+      height: 12px
+      bg-image('bulletin')
+      background-size: 22px 12px
+      background-repeat: no-repeat
+
+    .bulletin-text
+      vertical-align: top
+      margin: 0 4px
+      font-size: 10px
+
+    .icon-keyboard_arrow_right
+      position: absolute
+      font-size: 10px
+      right: 12px
+      top: 8px
+
+  .background
+    img
+      position: absolute
+      top: 0
+      left: 0
+      width: 100%
+      height: 100%
+      z-index: -1
+      filter: blur(10px)
 </style>
