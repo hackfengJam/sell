82d4e6566dc820c4eb38ee4db186f1c691bfc97c
HackFun <hackfeng624@gmail.com>
2021-07-11 23:53:48 +0800

    feat: 7-1~7-5

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -154,7 +154,7 @@ export default {
           width: 12px
           height: 12px
           margin-right: 4px
-          background-size: 12px
+          background-size: 12px 12px
           background-repeat: no-repeat
 
           &.decrease
